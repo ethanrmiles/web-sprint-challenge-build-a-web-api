8f51344803453d790b7ac01dd745e043901dfe1d
Ethan Miles <ethanmilescs@gmail.com>
2022-04-15 13:29:10 -0500

    /projects/:id now performs a put operation

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -17,14 +17,11 @@ router.get('/:id', (req,res ) => {
     })
 })
 
-router.get('/:id/actions', (req,res ) => {
-    // const id = req.params.id
-    // Projects.get(id)
-    // .then(action => {
-    //     res.json(action)
-    // })
-    res.send(' get/:id/actions route hit')
-})
+// router.get('/:id/actions', (req,res ) => {
+//     Projects.get(req.params.id)
+//     .then(action => {
+//         res.json(action)
+// })
 
 router.post('/', (req,res ) => {
     // Projects.insert(req.body)
@@ -35,16 +32,15 @@ router.post('/', (req,res ) => {
 })
 
 router.put('/:id', (req,res ) => {
-    // const changes = req.body
-    // Projects.update(req.params.id, changes)
-    // .then(action => {
-    //     if (action){
-    //         res.json(action)
-    //     }else {
-    //         res.json({ message: "something went wrong"})
-    //     }
-    // })
-    res.send(' put/ route hit')
+    const changes = req.body
+    Projects.update(req.params.id, changes)
+    .then(action => {
+        if (action){
+            res.json(action)
+        }else {
+            res.json({ message: "something went wrong"})
+        }
+    })
 })  
 
 router.delete('/:id', (req,res ) => {
