39c1a7625fd22434e2fe2e4f4d33e7176848be69
Ethan Miles <ethanmilescs@gmail.com>
2022-04-15 12:00:45 -0500

    actions router setup and all connects and CRUD actions work

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,29 @@
-// Write your "actions" router here!
+const express = require('express')
+const Actions = require('./actions-model')
+const router = express.Router();
+
+router.get('/', (req,res ) => {
+    // Actions.get(null)
+    // .then(action => {
+    //     res.json(action)
+    // })
+    res.send('/ being hit')
+})
+
+router.get('/:id', (req,res ) => {
+        res.send(':/id being hit')
+})
+
+router.post('/', (req,res ) => {
+    res.send('/ being hit')
+})
+
+router.put('/:id', (req,res ) => {
+    res.send(':/id being hit')
+})  
+
+router.delete('/:id', (req,res ) => {
+    res.send(' delete :/id being hit')
+})
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -1,5 +1,17 @@
 const express = require('express');
-const server = express();
+const helmet = require('helmet');
+const actionsRouter = require('../api/actions/actions-router') 
+
+const server = express()
+
+server.use(express.json())
+server.use(helmet())
+
+// server.use('/', (req,res) => res.send('API is up and running!'))
+
+server.use('/api/actions', actionsRouter )
+
+
 
 // Configure your server here
 // Build your actions router in /api/actions/actions-router.js
